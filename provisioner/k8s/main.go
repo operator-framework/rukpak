9615ba78205ee97b35b748071bf20df9557ca590
timflannagan <timflannagan@gmail.com>
2022-03-01 22:57:56 -0500

    k8s: Update the default unpack container image
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- provisioner/k8s/main.go
+++ provisioner/k8s/main.go
@@ -60,7 +60,7 @@ func main() {
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&systemNamespace, "system-namespace", "rukpak-system", "Configures the namespace that gets used to deploy system resources.")
-	flag.StringVar(&unpackImage, "unpack-image", "quay.io/tflannag/unpack:latest", "Configures the container image that gets used to unpack Bundle contents.")
+	flag.StringVar(&unpackImage, "unpack-image", "quay.io/operator-framework/k8s-provisioner:latest", "Configures the container image that gets used to unpack Bundle contents.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
