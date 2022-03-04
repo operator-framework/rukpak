fbf62e3a6bb01d4e4334ae81879796bfd8927cbd
Tyler Slaton <tyslaton@redhat.com>
2022-03-04 11:57:03 -0500

    test(unit): add envtest support and package for creating a client with it

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -6,5 +6,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" // Better linting
 	_ "github.com/onsi/ginkgo/ginkgo"                       // For running E2E tests
+	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"  // Generate deepcopy, conversion, and CRDs
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"     // Generate deepcopy, conversion, and CRDs
 )
