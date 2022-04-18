4b42940993271652864264ac531c4077430765e5
Tyler Slaton <tyslaton@redhat.com>
2022-04-18 12:19:59 -0400

    style: move NewTestingCRD into util package and add NewTestingCR function
    Signed-off-by: Tyler Slaton <tyslaton@redhat.com>

---
+++ internal/util/kubestructs.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"fmt"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func NewTestingCRD(name, group string, unique bool, versions []apiextensionsv1.CustomResourceDefinitionVersion) *apiextensionsv1.CustomResourceDefinition {
+	if unique {
+		name = GenName(name)
+	}
+	return &apiextensionsv1.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%v.%v", name, group),
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Scope:    apiextensionsv1.ClusterScoped,
+			Group:    group,
+			Versions: versions,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
+				Plural:   name,
+				Singular: name,
+				Kind:     name,
+				ListKind: name + "List",
+			},
+		},
+	}
+}
+
+func NewTestingCR(name, group, version, kind string) *unstructured.Unstructured {
+	newTestingCr := &unstructured.Unstructured{}
+	newTestingCr.SetKind(kind)
+	newTestingCr.SetAPIVersion(group + "/" + version)
+	newTestingCr.SetGenerateName(name)
+	return newTestingCr
+}
