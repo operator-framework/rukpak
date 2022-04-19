472d6ee2a0a48c5966f17452ecf3f2991a24a21d
Tyler Slaton <tyslaton@redhat.com>
2022-04-19 13:35:16 -0400

    test: add CrdReady function to the testutil package
    Signed-off-by: Tyler Slaton <tyslaton@redhat.com>

--- test/testutil/structs.go
+++ test/testutil/structs.go
@@ -8,6 +8,8 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
+// NewTestingCRD takes a name, group and versions to be set for a new CRD. If name is set to ""
+// then it will run testutil.GenName() on it with the DefaultCrdName.
 func NewTestingCRD(name, group string, versions []apiextensionsv1.CustomResourceDefinitionVersion) *apiextensionsv1.CustomResourceDefinition {
 	if name == "" {
 		name = GenName(DefaultCrdName)
@@ -30,10 +32,33 @@ func NewTestingCRD(name, group string, versions []apiextensionsv1.CustomResource
 	}
 }
 
+// NewTestingCR takes a name, group, version and king in order to create a
+// new CR of a resource correctly.
 func NewTestingCR(name, group, version, kind string) *unstructured.Unstructured {
 	newTestingCr := &unstructured.Unstructured{}
 	newTestingCr.SetKind(kind)
 	newTestingCr.SetAPIVersion(group + "/" + version)
 	newTestingCr.SetGenerateName(name)
 	return newTestingCr
+}
+
+// CrdReady takes a CRD's status and determines if it is ready to accept new CRs
+func CrdReady(status *apiextensionsv1.CustomResourceDefinitionStatus) bool {
+	if status == nil {
+		return false
+	}
+	established, namesAccepted := false, false
+	for _, cdt := range status.Conditions {
+		switch cdt.Type {
+		case apiextensionsv1.Established:
+			if cdt.Status == apiextensionsv1.ConditionTrue {
+				established = true
+			}
+		case apiextensionsv1.NamesAccepted:
+			if cdt.Status == apiextensionsv1.ConditionTrue {
+				namesAccepted = true
+			}
+		}
+	}
+	return established && namesAccepted
 }
