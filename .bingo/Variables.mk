a6940a1565039ba5045ca171e53d9d22abef7890
dtfranz <dfranz@redhat.com>
2023-06-28 15:06:42 -0700

    Bingo version updates
    Updates all bingo tools to use go 1.20 and adds yq.
    
    Signed-off-by: dtfranz <dfranz@redhat.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -65,3 +65,9 @@ $(SETUP_ENVTEST): $(BINGO_DIR)/setup-envtest.mod
 	@echo "(re)installing $(GOBIN)/setup-envtest-v0.0.0-20220607150856-0c2effbc7eab"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=setup-envtest.mod -o=$(GOBIN)/setup-envtest-v0.0.0-20220607150856-0c2effbc7eab "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 
+YQ := $(GOBIN)/yq-v4.34.1
+$(YQ): $(BINGO_DIR)/yq.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/yq-v4.34.1"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=yq.mod -o=$(GOBIN)/yq-v4.34.1 "github.com/mikefarah/yq/v4"
+
