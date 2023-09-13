fb2cac87adfd322c433be0bc6a693656cc23935a
Joaquim Moreno Prusi <joaquim@redhat.com>
2023-09-13 13:02:24 +0200

    Adds BundleDeploymentHealth feature gate
    Signed-off-by: Joaquim Moreno Prusi <joaquim@redhat.com>

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -6,13 +6,17 @@ import (
 )
 
 const (
-// Add new feature gates constants (strings)
-// Ex: SomeFeature featuregate.Feature = "SomeFeature"
+	// Add new feature gates constants (strings)
+	// Ex: SomeFeature featuregate.Feature = "SomeFeature"
+
+	BundleDeploymentHealth featuregate.Feature = "BundleDeploymentHealth"
 )
 
 var rukpakFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	// Add new feature gate definitions
 	// Ex: SomeFeature: {...}
+
+	BundleDeploymentHealth: {Default: false, PreRelease: featuregate.Alpha},
 }
 
 var RukpakFeatureGate featuregate.MutableFeatureGate = featuregate.NewFeatureGate()
