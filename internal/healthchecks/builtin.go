24a4e980bc94bd16317a6631641801f5502ffc6d
Ankita Thomas <ankithom@redhat.com>
2023-10-16 13:46:53 -0400

    use k8s cli-utils health checks for bundledeployment resources
    Signed-off-by: Ankita Thomas <ankithom@redhat.com>

--- internal/healthchecks/builtin.go
+++ internal/healthchecks/builtin.go
@@ -5,13 +5,11 @@ import (
 	"errors"
 	"fmt"
 
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -27,6 +25,10 @@ import (
 // - Pods
 // - APIServices
 // - CustomResourceDefinitions
+// - Jobs
+// - Services
+// - PersistentVolumeClaims
+// - PodDisruptionBudgets
 //
 // If the resource is not supported, it is assumed to be healthy.
 func AreObjectsHealthy(ctx context.Context, client client.Client, objects []client.Object) error {
@@ -45,147 +47,38 @@ func AreObjectsHealthy(ctx context.Context, client client.Client, objects []clie
 			continue
 		}
 
-		switch u.GroupVersionKind() {
-		case appsv1.SchemeGroupVersion.WithKind("Deployment"):
-			// Check if the deployment is available.
-			obj := &appsv1.Deployment{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			conditionExists := false
-			for _, condition := range obj.Status.Conditions {
-				if condition.Type == appsv1.DeploymentAvailable {
-					if condition.Status != "True" {
-						gvkErrors = appendResourceError(gvkErrors, obj, condition.Message)
-					}
-					conditionExists = true
-					break
-				}
-			}
-			if conditionExists {
-				continue
-			}
-			gvkErrors = appendResourceError(gvkErrors, obj, "DeploymentAvailable condition not found")
-		case appsv1.SchemeGroupVersion.WithKind("StatefulSet"):
-			// This logic has been adapted from the helm codebase.
-			obj := &appsv1.StatefulSet{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			// This logic has been adapted from the helm codebase.
-			// - https://github.com/helm/helm/blob/e7bb860d9a32e8739c944b8e7b7f7031d752411a/pkg/kube/ready.go#L357-L410
-
-			// If the statefulset is not using the RollingUpdate strategy, we assume it's healthy.
-			if obj.Spec.UpdateStrategy.Type != appsv1.RollingUpdateStatefulSetStrategyType {
-				continue
-			}
-			if obj.Status.ObservedGeneration < obj.Generation {
-				gvkErrors = appendResourceError(gvkErrors, obj, "StatefulSet is not ready (update has not yet been observed)")
-			}
-
-			var partition int
-			var replicas = 1
-			if obj.Spec.UpdateStrategy.RollingUpdate != nil && obj.Spec.UpdateStrategy.RollingUpdate.Partition != nil {
-				partition = int(*obj.Spec.UpdateStrategy.RollingUpdate.Partition)
-			}
-			if obj.Spec.Replicas != nil {
-				replicas = int(*obj.Spec.Replicas)
-			}
-			expectedReplicas := replicas - partition
-
-			if obj.Status.UpdatedReplicas < int32(expectedReplicas) {
-				gvkErrors = appendResourceError(gvkErrors, obj, fmt.Sprintf("StatefulSet is not ready (expected %d replicas, got %d)", expectedReplicas, obj.Status.UpdatedReplicas))
-				continue
-			}
-			if int(obj.Status.ReadyReplicas) != replicas {
-				gvkErrors = appendResourceError(gvkErrors, obj, fmt.Sprintf("StatefulSet is not ready (expected %d replicas, got %d)", replicas, obj.Status.ReadyReplicas))
-				continue
-			}
-			if partition == 0 && obj.Status.CurrentRevision != obj.Status.UpdateRevision {
-				gvkErrors = appendResourceError(gvkErrors, obj, fmt.Sprintf("StatefulSet is not ready (expected revision %s, got %s)", obj.Status.CurrentRevision, obj.Status.UpdateRevision))
-				continue
-			}
-		case appsv1.SchemeGroupVersion.WithKind("DaemonSet"):
-			// Check if the daemonset is ready.
-			obj := &appsv1.DaemonSet{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			if obj.Status.NumberAvailable != obj.Status.DesiredNumberScheduled {
-				gvkErrors = appendResourceError(gvkErrors, obj, "DaemonSet is not ready")
-			}
-		case appsv1.SchemeGroupVersion.WithKind("ReplicaSet"):
-			// Check if the replicaset is ready.
-			obj := &appsv1.ReplicaSet{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			if obj.Status.AvailableReplicas != obj.Status.Replicas {
-				gvkErrors = appendResourceError(gvkErrors, obj, "ReplicaSet is not ready")
-			}
-		case corev1.SchemeGroupVersion.WithKind("Pod"):
-			// Check if the pod is running or succeeded.
-			obj := &corev1.Pod{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			if obj.Status.Phase != corev1.PodRunning && obj.Status.Phase != corev1.PodSucceeded {
-				gvkErrors = appendResourceError(gvkErrors, obj, "Pod is not Running or Succeeded")
-			}
-		case apiregistrationv1.SchemeGroupVersion.WithKind("APIService"):
-			// Check if the APIService is available.
+		if object.GetObjectKind().GroupVersionKind() == apiregistrationv1.SchemeGroupVersion.WithKind("APIService") {
 			obj := &apiregistrationv1.APIService{}
 			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
 				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
 				continue
 			}
-			conditionExists := false
-			for _, condition := range obj.Status.Conditions {
+
+			// Check if the APIService is available.
+			var isAvailable *apiregistrationv1.APIServiceCondition
+			for i, condition := range obj.Status.Conditions {
 				if condition.Type == apiregistrationv1.Available {
-					if condition.Status != "True" {
-						gvkErrors = appendResourceError(gvkErrors, obj, condition.Message)
-					}
-					conditionExists = true
+					isAvailable = &obj.Status.Conditions[i]
 					break
 				}
 			}
-			if conditionExists {
-				continue
+			if isAvailable == nil {
+				gvkErrors = appendResourceError(gvkErrors, obj, "Available condition not found")
+			} else if isAvailable.Status == apiregistrationv1.ConditionFalse {
+				gvkErrors = appendResourceError(gvkErrors, obj, isAvailable.Message)
 			}
-			// If we are here we didn't find the "Available" condition, so we assume the APIService is non healthy.
-			gvkErrors = appendResourceError(gvkErrors, obj, "Available condition not found")
-		case apiextensionsv1.SchemeGroupVersion.WithKind("CustomResourceDefinition"):
-			// Check if the CRD is established.
-			obj := &apiextensionsv1.CustomResourceDefinition{}
-			if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
-				gvkErrors = appendResourceError(gvkErrors, obj, err.Error())
-				continue
-			}
-			conditionExists := false
-			for _, condition := range obj.Status.Conditions {
-				if condition.Type == apiextensionsv1.Established {
-					if condition.Status != "True" {
-						gvkErrors = appendResourceError(gvkErrors, obj, condition.Message)
-					}
-					conditionExists = true
-					break
-				}
-			}
-			if conditionExists {
-				continue
-			}
-			gvkErrors = appendResourceError(gvkErrors, obj, "Established condition not found")
-		default:
-			// If we don't know how to check the health of the object, we assume it's healthy.
 			continue
 		}
-	}
 
+		result, err := status.Compute(u)
+		if err != nil {
+			gvkErrors = appendResourceError(gvkErrors, object, err.Error())
+		}
+
+		if result.Status != status.CurrentStatus {
+			gvkErrors = appendResourceError(gvkErrors, object, fmt.Sprintf("object %s: %s", result.Status, result.Message))
+		}
+	}
 	return errors.Join(gvkErrors...)
 }
 
